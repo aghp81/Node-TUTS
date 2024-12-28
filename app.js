8be7bc184eb053398526ad723aeefbfeadb3c4e2
aghp81 <aghp81@gmail.com>
2024-12-28 13:19:47 +0330

    ایجاد سرور

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const http = require("http");
+
 console.log('Node.js نصب شد و کار می‌کند!');
 
 
@@ -7,3 +9,16 @@ console.log("سلام دنیا! این اولین برنامه Node.js من اس
 // یک عملیات ساده ریاضی
 let sum = 5 + 7;
 console.log("مجموع 5 و 7 برابر است با:", sum);
+
+
+// ایجاد سرور
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { "Content-Type": "text/plain; charset=utf-8" });
+    res.end("سلام! این اولین سرور Node.js من است!");
+});
+
+// سرور روی پورت 3000 اجرا می‌شود
+server.listen(3000, () => {
+    console.log("سرور در حال اجرا است: http://localhost:3000");
+});
+
