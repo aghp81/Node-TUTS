df8e470c80037dfc27dd6d2016ee9db293eb207a
aghp81 <aghp81@gmail.com>
2025-01-02 17:31:40 +0330

    comment

--- Read_JSON_File.js
+++ Read_JSON_File.js
@@ -1,5 +1,8 @@
 const fs = require('fs');
 
+// فایل data.json به صورت متنی خوانده می‌شود.
+// سپس با JSON.parse محتوای متنی به یک شیء جاوااسکریپت تبدیل می‌شود.
+
 fs.readFile('data.json', 'utf8', (err, data) => {
     if (err) {
         console.error('خطا در خواندن فایل:', err);
