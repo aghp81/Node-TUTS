827ced1dc27e246dee27ba7f57a1b01cd0f8c75f
aghp81 <aghp81@gmail.com>
2025-01-03 15:17:16 +0330

    توضیحات

--- Rm.js
+++ Rm.js
@@ -2,6 +2,10 @@ const fs = require('fs');
 
 // حذف فولدرها با محتویات غیرهمزمان
 
+// recursive: true: تمامی فایل‌ها و پوشه‌های داخل فولدر را حذف می‌کند.
+// force: true: حذف بدون بررسی مجوزها.
+
+
 fs.rm('myFolder', { recursive: true, force: true }, (err) => {
     if (err) {
         console.error('خطا در حذف پوشه:', err.message);
